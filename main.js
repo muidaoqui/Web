2c91d5188feabe415c2040465825aeefee3c3a59
muidaoqui <muidao156@gmail.com>
2025-04-14 22:06:31 +0700

    Buoi 11

--- main.js
+++ main.js
@@ -1,51 +1,5 @@
-
-
-const tongStartEnd = (start, end) => {
-    let startEnd = 0;
-    for (let i = start; i <= end; i++) {
-        startEnd += i;
-    }
-    return startEnd;
-};
-
-const tachTu = (str) => {
-    let words = [];
-    let word = '';
-    for(let char of str){
-        if (char === ' '){
-            if (word) {
-                words.push(word);
-                word = '';
-            }
-        } else {
-            word += char;
-        }
-    }
-    if (word) {
-        words.push(word);
-    }
-    return words;
-}
-
-const swap = (arr) => {
-    for(let i=0; i < arr.length; i++){
-        for(let j=i+1; j < arr.length; j++){
-            if(arr[i] > arr[j]){
-                let temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-    }
-}
-
-const checkDoiXung = (num) => {
-    const str = num.toString();
-    if (str.length % 2 === 0) {
-        for(let i = 0; i < str.length; i++) {
-            if (str[i] === str[str.length - 1 - i]) {
-                return true;
-            } return false;
-        }
-    } else return false;
-}
+let arr = [1, 2, 3];
+let newArr = arr.find((item)=>{
+    return item * 2;
+});
+console.log(newArr); // Output: [2, 4, 6]
